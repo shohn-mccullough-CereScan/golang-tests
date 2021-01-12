7cc1addddcff416b0313132df103c41ff16b9b1d
lewShohn1 <42552246+lewShohn1@users.noreply.github.com>
2021-01-12 13:08:36 -0700

    Initial Upload
    Small function to accept CLI Arguments when running the executable.

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+//Input ... structure for data input to the program
+type Input struct {
+	Environment string `json:"environment"`
+	App         string `json:"app"`
+}
+
+func main() {
+	var input Input
+	b, err := json.Marshal(os.Args[0])
+	if err != nil {
+		fmt.Println("ERROR:", err)
+	}
+
+	json.Unmarshal(b, &input)
+
+	output, err := ingestInput(input)
+	if err != nil {
+		fmt.Println("main() Ending Error:", err)
+	}
+
+	fmt.Println(output)
+} //End main()
+
+func ingestInput(input Input) (string, error) {
+	var err error
+	var output string
+	env := input.Environment
+	app := input.App
+
+	//Check Environment
+	if len(env) <= 0 {
+		err = errors.New("Please provide a valid environment value")
+	} else {
+		output = output + "Current Environment is " + env + "\n"
+	}
+
+	//Check App
+	if len(app) <= 0 {
+		err = errors.New("Please provide a valid application value")
+	} else {
+		output = output + "Current Environment is " + app
+	}
+
+	return output, err
+} //End ingestInput()
